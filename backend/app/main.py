b42ae1a4d9cdbd8f3eed23580ee6ce72caf957d2
Fehizoro <rakotoarisoafehizoro@gmail.com>
2026-09-23 16:04:22 +0300

    Correction accès application depuis la VM

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8081"],
+    allow_origins=[
+        "http://localhost:8081",
+        "http://192.168.56.102:8081",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
